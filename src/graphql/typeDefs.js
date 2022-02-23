f66dbe1bfa9bd655b0093b06ecb6d9bec39abd44
eduardonk9999 <thiago.novak7@gmail.com>
2022-02-23 10:46:49 -0300

    Add, graphql, mongo conexao

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -1,5 +1,9 @@
-export default`
-  type Query {
-    hello: String
-  }
-`;
+import { fileLoader, mergeTypes } from "merge-graphql-schemas";
+import path from 'path';
+
+const typesArry = fileLoader(path.join(__dirname, 'modules', '**', '*.gql'));
+const typeDefs = mergeTypes(typesArry)
+
+console.log(typeDefs)
+
+export default typeDefs
